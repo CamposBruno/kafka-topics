3f7f1cfabcf73053c54dda209559a3ab930e333d
Bruno Campos <bhncampos@gmail.com>
2020-09-10 16:14:58 -0300

    update

--- src/dto.ts
+++ src/dto.ts
@@ -1,17 +1,21 @@
 class SendTransaction {
+    activityId: string
     amount: string
     recipient: string
     walletId: number
     approved: boolean
+    status: string
     createdAt: Date
     updatedAt: Date
 }
 
 export class SendTransactionBTC extends SendTransaction {    
+    txid?: string
     statoshis?: string
 }
 
 export class SendTransactionETH extends SendTransaction {    
+    hash?: string
     gas?: string
     gasLimit?: string
 }
